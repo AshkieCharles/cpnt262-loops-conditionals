72bcdfa9cbe0a80613e7c819e4fb251fc2c0fcd8
Ashkie <ashkiecharles@gmail.com>
2023-10-10 20:00:03 -0600

    added comments

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 let firstNumb = 0;
 let secondNumb = 0;
 while (firstNumb < 5) {
+/* 1 */
   firstNumb++;
   secondNumb += firstNumb;
+  /*comment 1 + 2 + 3*/
 }
 console.log(firstNumb)
 console.log(secondNumb)
+
+secondNumb += firstNumb 
