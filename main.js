c3462e288761d2ed5592aec6838c98945e942f03
Ashkie Esperanza <ashkiecharles@gmail.com>
2023-10-18 09:45:50 -0600

    added new variable names

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const reverseString = "alucarD";
 
-let reversedString = '';
+let newString = '';
 /* The minus one is to prevent an undefined from forming as the length of the string is larger than the original string we had. */
 /* The loop keeps going until the every letter of the string is reversed. */
 for (let i = reverseString.length - 1; i >= 0; i--) {
   /* The new string will have its letters replaced by whatever letter is in position i */
-  reversedString += reverseString[i];
+  newString += reverseString[i];
 }
 
-console.log(reversedString);
+console.log(newString);
 /* Break */
 
 /* This will be the sentence we will use to find the longest word, the answer should be predicament */
