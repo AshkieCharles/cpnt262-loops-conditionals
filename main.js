e9c9a7561dec2881658eaac1b2d84f9468603ce7
Ashkie <ashkiecharles@gmail.com>
2023-10-22 21:14:43 -0600

    added a few tweaks

--- main.js
+++ main.js
@@ -4,9 +4,18 @@ while (firstNumb < 5) {
 /* 1 */
   firstNumb++;
   secondNumb += firstNumb;
-  /*comment 1 + 2 + 3*/
+ /*
+secondNumb = (0 + 1)
+secondNumb = (1 + 2)
+secondNumb = (3 + 3)
+secondNumb = (6 + 4)
+secondNumb = (10 + 5) 
+*/
 }
 console.log(firstNumb)
 console.log(secondNumb)
 
-secondNumb += firstNumb 
+secondNumb = (secondNumb + firstNumb)
+
+
+
